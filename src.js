324a89184da9e737e144900e9dc1bc1ad5a2973d
jcelisca <90579586+jcelisca@users.noreply.github.com>
2021-11-22 10:09:07 -0500

    Creación de funciones

---
+++ src.js
@@ -0,0 +1,10 @@
+const calcularDepreciacionNIIF = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+
+}
+
+const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+    
+}
+
+module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
+module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
