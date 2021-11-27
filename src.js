3693b4ef49195a3168402a882ee9b63da723b7df
jcelisca <90579586+jcelisca@users.noreply.github.com>
2021-11-27 09:53:48 -0500

    Funcion mostrar productos

--- src.js
+++ src.js
@@ -18,5 +18,21 @@ const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil,
     return (calcularDepreciacionNIIF(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar))/3778;
 }
 
+async function mostrarProductos(){
+	let response = await fetch(
+		"https://misiontic2022upb.vercel.app/api/logistics/products"
+	);
+	let prosuctosAPI = await response.json();
+}
+
+async function mostrarProductosPrecioDolares(){
+	let response = await fetch(
+		"https://misiontic2022upb.vercel.app/api/logistics/products"
+	);
+	let prosuctosAPI = await response.json();
+}
+
+module.exports.mostrarProductos = mostrarProductos;
+module.exports.mostrarProductosPrecioDolares = mostrarProductosPrecioDolares;
 module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
 module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
