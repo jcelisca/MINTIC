9cb5c16b3a1c8eb368a9c6767c7d14e162fdaec3
jcelisca <90579586+jcelisca@users.noreply.github.com>
2021-11-28 13:52:49 -0500

    mostrar lista de productos con depreciacion

--- src.js
+++ src.js
@@ -11,28 +11,58 @@ const calcularDepreciacionNIIF = (precioInicial, precioFinal, vidaUtil, numeroPe
     }
 }
 
-const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+/*const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
 	if(precioInicial<0){
 		throw "error";
 	}
     return (calcularDepreciacionNIIF(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar))/3778;
-}
+}*/
 
 
 async function mostrarProductos(){
-	let response = await fetch("https://misiontic2022upb.vercel.app/api/logistics/products")	
-	let productosAPI = await response.json();
-	//console.log(productosAPI);
-}
+    let productosConDepreciacion = [];
+	let response = await fetch("https://misiontic2022upb.vercel.app/api/logistics/products");
+    let productosAPI = await response.json()	
 
-mostrarProductos();
+    productosAPI.forEach(e => {
+        const precioDepreciado = calcularDepreciacionNIIF(e.precioInicial,e.precioFinal,e.vidaUtil,e.periodo_consultado);
+        productosConDepreciacion.push({
+            "depreciacion":precioDepreciado,
+            "precioInicial":e.precioInicial,
+            "precioFinall":e.precioFinal,
+            "vidaUtil":e.vidaUtil,
+            "periodo_consultado":e.periodo_consultado
+        })
+    });
+
+    return productosConDepreciacion;
+}
 
 async function mostrarProductosPrecioDolares(){
-	let response = await fetch("https://misiontic2022upb.vercel.app/api/logistics/products");
-	let prosuctosAPI = await response.json();
+    let productosConDepreciacion = [];
+	let response = await fetch('https://misiontic2022upb.vercel.app/api/logistics/products');
+	let productosAPI = await response.json()
+
+    productosAPI.forEach(e => {
+        let preciod =[];
+        const depreciaciond = calcularDepreciacionNIIF(e.precioInicial,e.precioFinal,e.vidaUtil,e.periodo_consultado);
+        fetch('https://misiontic2022upb.vercel.app/api/logistics/to-dolar-converter/'+depreciaciond)
+        .then(res => res.json())
+        .then(data => preciod.push(data))
+        console.log(preciod)
+        productosConDepreciacion.push({
+            "depreciacion":preciod,
+            "precioInicial":e.precioInicial,
+            "precioFinall":e.precioFinal,
+            "vidaUtil":e.vidaUtil,
+            "periodo_consultado":e.periodo_consultado
+        })
+    });
+    
+    return productosConDepreciacion;
 }
 
-/*module.exports.mostrarProductos = mostrarProductos;
+module.exports.mostrarProductos = mostrarProductos;
 module.exports.mostrarProductosPrecioDolares = mostrarProductosPrecioDolares;
-module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
-module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;*/
+//module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
+//module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
