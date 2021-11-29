b38956bb8683a00a4d93b14f27f11b7caf209906
jcelisca <90579586+jcelisca@users.noreply.github.com>
2021-11-29 12:42:51 -0500

    Correccion a función depreciacion en dolares

--- src.js
+++ src.js
@@ -38,31 +38,33 @@ async function mostrarProductos(){
     return productosConDepreciacion;
 }
 
+
+
 async function mostrarProductosPrecioDolares(){
     let productosConDepreciacion = [];
 	let response = await fetch('https://misiontic2022upb.vercel.app/api/logistics/products');
 	let productosAPI = await response.json()
 
-    productosAPI.forEach(e => {
-        let preciod =[];
-        const depreciaciond = calcularDepreciacionNIIF(e.precioInicial,e.precioFinal,e.vidaUtil,e.periodo_consultado);
-        fetch('https://misiontic2022upb.vercel.app/api/logistics/to-dolar-converter/'+depreciaciond)
-        .then(res => res.json())
-        .then(data => preciod.push(data))
-        console.log(preciod)
+    for(var i = 0 ; i < productosAPI.length; i++) {
+        const depreciaciond = calcularDepreciacionNIIF(productosAPI[i].precioInicial,productosAPI[i].precioFinal,productosAPI[i].vidaUtil,productosAPI[i].periodo_consultado);
+        let valorDepreciacion = await fetch('https://misiontic2022upb.vercel.app/api/logistics/to-dolar-converter/'+depreciaciond)
+        let precioDolares = await valorDepreciacion.json();
+
         productosConDepreciacion.push({
-            "depreciacion":preciod,
-            "precioInicial":e.precioInicial,
-            "precioFinall":e.precioFinal,
-            "vidaUtil":e.vidaUtil,
-            "periodo_consultado":e.periodo_consultado
+            "depreciacion":precioDolares,
+            "precioInicial":productosAPI[i].precioInicial,
+            "precioFinall":productosAPI[i].precioFinal,
+            "vidaUtil":productosAPI[i].vidaUtil,
+            "periodo_consultado":productosAPI[i].periodo_consultado
         })
-    });
+    };
     
     return productosConDepreciacion;
 }
 
-module.exports.mostrarProductos = mostrarProductos;
-module.exports.mostrarProductosPrecioDolares = mostrarProductosPrecioDolares;
+mostrarProductos().then(console.log);
+mostrarProductosPrecioDolares().then(console.log);
+//module.exports.mostrarProductos = mostrarProductos;
+//module.exports.mostrarProductosPrecioDolares = mostrarProductosPrecioDolares;
 //module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
 //module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
